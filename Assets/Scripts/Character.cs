43d073d021e5781226f8a9ebc594256ed22e9f39
Sercan NAYA <55290105+sercannaya@users.noreply.github.com>
2021-02-16 21:26:21 +0300

    Initial commit

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    //NICKNAME
+    //Scriptable Object STATS
+    //State
+
+}
---
+++ Assets/Scripts/CharacterAnimationController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour
+{
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void PlayJumpingAnimation()
+    {
+        animator.SetBool("isJumping", true);
+    }
+
+    public void PlayIdleAnimation()
+    {
+        animator.SetBool("isRunning", false);
+        animator.SetBool("isJumping", false);
+    }
+
+    public void PlayRunningAnimation()
+    {
+        animator.SetBool("isRunning", true);
+        animator.SetBool("isJumping", false);
+    }
+
+
+}
